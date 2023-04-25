92aa62d9cf9131d79065579a95f656d1c9488e30
Jason <jasontan20030829@gmail.com>
2023-04-25 05:50:54 +0100

    Changing Java Script directories
    Changing all Java Script directories

--- Jobhive.js
+++ Jobhive.js
@@ -1,3 +1,20 @@
+const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const submit = document.querySelector('.submbutton input');
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
+
+submit.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
 const carousel = document.querySelector(".carousel");
 firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".quiz-slider i");
